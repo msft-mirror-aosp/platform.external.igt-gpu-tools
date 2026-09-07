9949fcc0e451b16fe0ac5468afcb50e276d5aad3
Lukasz Laguna <lukasz.laguna@intel.com>
2026-09-07 14:55:37 +0200

    tests/intel/xe_sriov_ggtt: Install SR-IOV exit handler
    Install SR-IOV exit handler in the test to disable VFs and restore
    driver autoprobe on exit.
    
    Signed-off-by: Lukasz Laguna <lukasz.laguna@intel.com>
    Reviewed-by: Marcin Bernatowicz <marcin.bernatowicz@linux.intel.com>

--- tests/intel/xe_sriov_ggtt.c
+++ tests/intel/xe_sriov_ggtt.c
@@ -701,6 +701,7 @@ int igt_main_args("", long_opts, help_str, opts_handler, NULL)
 		igt_require(igt_sriov_is_pf(pf_fd));
 		igt_require(igt_sriov_get_enabled_vfs(pf_fd) == 0);
 		autoprobe = igt_sriov_is_driver_autoprobe_enabled(pf_fd);
+		igt_sriov_install_exit_handler(pf_fd, NULL, NULL);
 	}
 
 	igt_describe("Verify that the VF ID in the GGTT PTE has been correctly assigned");
@@ -752,6 +753,7 @@ int igt_main_args("", long_opts, help_str, opts_handler, NULL)
 			    igt_sriov_disable_driver_autoprobe(pf_fd);
 		igt_abort_on_f(autoprobe != igt_sriov_is_driver_autoprobe_enabled(pf_fd),
 			       "Failed to restore sriov_drivers_autoprobe value\n");
+		igt_sriov_clear_exit_handler();
 		drm_close_driver(pf_fd);
 	}
 }
